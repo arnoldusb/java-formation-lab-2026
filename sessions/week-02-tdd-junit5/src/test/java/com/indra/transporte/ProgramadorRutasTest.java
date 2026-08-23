dd0d7c0f825cabc42c58bc37301674607afef5a5
JAMES-PC\yiseb <yisebell@gmail.com>
2026-08-23 12:37:28 -0500

    test: debe registrar un horario

---
+++ sessions/week-02-tdd-junit5/src/test/java/com/indra/transporte/ProgramadorRutasTest.java
@@ -0,0 +1,27 @@
+package com.indra.transporte;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import com.indra.transporte.model.Bus;
+import com.indra.transporte.model.Horario;
+import com.indra.transporte.model.Ruta;
+
+public class ProgramadorRutasTest {
+    @Test
+    @DisplayName("Debe registrar un horario")
+    void debeRegistrarUnHorario() {
+        Bus bus = new Bus("ABC123", "Autobus");
+        Ruta ruta = new Ruta("R001", "Ciudad A", "Ciudad B");
+        Horario horario = new Horario(bus, ruta,
+                java.time.LocalTime.of(8, 0), java.time.LocalTime.of(10, 0));
+
+        ProgramadorRutas programador = new ProgramadorRutas();
+
+        programador.programar(horario);
+
+        assertEquals(1, programador.consultarTodos().size());
+    }
+}
